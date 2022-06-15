ae5a764a1acb15a4a5c626d54860b66c50f45baa
bauele <bauele@users.noreply.github.com>
2022-06-15 11:37:42 -0400

    Add functionality to support item building from generic objects

--- src/itemBuilder.ts
+++ src/itemBuilder.ts
@@ -1,7 +1,15 @@
 import { Item } from './item';
 
 export class ItemBuilder {
-    buildItem(jsonString: string): Item {
+    buildItem(data: any) {
+        if (typeof data === 'string') {
+            return this.buildItemFromJSONString(data);
+        } else {
+            return this.buildItemFromObject(data);
+        }
+    }
+
+    buildItemFromJSONString(jsonString: string): Item {
         let item = new Item();
         let jsonObject = JSON.parse(jsonString);
 
@@ -48,6 +56,61 @@ export class ItemBuilder {
                 item.setServingSize(
                     Number.parseInt(jsonObject.servingSize.amount),
                     jsonObject.servingSize.unit
+                );
+            } catch (error) {
+                console.log('Attempted to set invalid quantity');
+            }
+        }
+
+        return item;
+    }
+
+    buildItemFromObject(object: any) {
+        let item = new Item();
+
+        if (object.id) {
+            item.setId(object.id);
+        }
+
+        if (object.name) {
+            item.setName(object.name);
+        }
+
+        if (object.brand) {
+            item.setBrand(object.brand);
+        }
+
+        if (object.calories) {
+            item.setCalories(Number.parseInt(object.calories));
+        }
+
+        if (object.vendorPrices) {
+            object.vendorPrices.forEach(
+                (jsonVendorPrice: { name: string; price: string }) => {
+                    item.addVendorPrice(
+                        jsonVendorPrice.name,
+                        Number.parseFloat(jsonVendorPrice.price)
+                    );
+                }
+            );
+        }
+
+        if (object.totalQuantity) {
+            try {
+                item.setTotalQuantity(
+                    Number.parseInt(object.totalQuantity.amount),
+                    object.totalQuantity.unit
+                );
+            } catch (error) {
+                console.log('Attempted to set invalid quantity');
+            }
+        }
+
+        if (object.servingSize) {
+            try {
+                item.setServingSize(
+                    Number.parseInt(object.servingSize.amount),
+                    object.servingSize.unit
                 );
             } catch (error) {
                 console.log('Attempted to set invalid quantity');
--- tests/itemBuilder.test.ts
+++ tests/itemBuilder.test.ts
@@ -23,3 +23,45 @@ test('Build item with proper string', async () => {
     expect(item.getTotalQuantity()).toEqual({ amount: 660, unit: 'g' });
     expect(item.getServingSize()).toEqual({ amount: 95, unit: 'g' });
 });
+
+test('Build item with proper object', async () => {
+    let object = {
+        _id: '62a9d9fb5644ae4d840373e1',
+        accountId: '6228ed6564ca692b08cd6085',
+        name: 'Bagel',
+        brand: 'Generic',
+        calories: 280,
+        vendorPrices: [
+            {
+                name: 'ShopA',
+                price: 3.42,
+            },
+            {
+                name: 'ShopC',
+                price: 5,
+            },
+        ],
+        totalQuantity: {
+            amount: 12,
+            unit: 'lb',
+        },
+        servingSize: {
+            amount: 1,
+            unit: 'lb',
+        },
+    };
+
+    let item = new Item();
+    let itemBuilder = new ItemBuilder();
+    item = itemBuilder.buildItem(object);
+
+    expect(item.getName()).toBe('Bagel');
+    expect(item.getBrand()).toBe('Generic');
+    expect(item.getCalories()).toBe(280);
+    expect(item.getVendorPrices()).toEqual([
+        { name: 'ShopA', price: 3.42 },
+        { name: 'ShopC', price: 5 },
+    ]);
+    expect(item.getTotalQuantity()).toEqual({ amount: 12, unit: 'lb' });
+    expect(item.getServingSize()).toEqual({ amount: 1, unit: 'lb' });
+});
